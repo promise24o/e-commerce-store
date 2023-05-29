e438a631c452dda8e48056ef56b8fce6fae32f8a
promise24o <promiseobe531@gmail.com>
2023-05-29 01:02:38 +0100

    --------

--- e-comerce-backend/backend/config/db.js
+++ e-comerce-backend/backend/config/db.js
@@ -1,21 +1,20 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require ('dotenv').config ()
+const mongoose = require ('mongoose')
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      process.env.MONGO_URI || 'mongodb://127.0.0.1/ecom',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
-    )
+    try {
+        await mongoose.connect (process.env.MONGO_URI || 'mongodb+srv://promiseobe532:1qaz2wsx@fashionfusion.jcpzafv.mongodb.net/', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        },)
 
-    console.log('MongoDB connection SUCCESS')
-  } catch (error) {
-    console.error('MongoDB connection FAIL')
-    process.exit(1)
-  }
+        console.log ('MongoDB connection SUCCESS')
+    } catch (error) {
+        console.error ('MongoDB connection FAIL')
+        process.exit (1)
+    }
 }
 
-module.exports = {connectDB}
+module.exports = {
+    connectDB
+}
